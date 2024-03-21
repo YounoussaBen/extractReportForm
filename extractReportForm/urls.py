657dbd896e79b427f43ef80028c6767972ff45d9
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-21 13:19:14 +0000

    permission

--- extractReportForm/urls.py
+++ extractReportForm/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("download/", include("police_report.urls")),
+     path("__reload__/", include("django_browser_reload.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
