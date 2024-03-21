9e334cd7b18f4547bebb0faead1af68eb5e4c41b
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-21 14:18:36 +0000

    Collectstatic files

--- extractReportForm/settings.py
+++ extractReportForm/settings.py
@@ -131,6 +131,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Define your STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
