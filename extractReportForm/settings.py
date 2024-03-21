3520b4e475a1188f5aff42edcbd681723f21c500
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-21 14:40:28 +0000

    allowed hostfixing

--- extractReportForm/settings.py
+++ extractReportForm/settings.py
@@ -33,8 +33,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
-
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST_LOCAL', 'localhost'),  # Default to localhost for development
+    os.getenv('ALLOWED_HOST_RENDER', ''),  # Default to empty string for production
+]
 
 # Application definition
 
