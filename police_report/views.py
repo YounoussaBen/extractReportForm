5ef122c75d87477aefca5d402f7fe74f9a80308a
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-21 13:19:30 +0000

    Update fields

--- police_report/views.py
+++ police_report/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-
 from django.http import HttpResponse
 from django.views.generic import View
 from .utils import render_to_pdf 
@@ -7,39 +6,50 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ApplicationSerializer
 from .models import Application  
+from rest_framework.permissions import IsAuthenticated
+
 
 class GeneratePdf(View):
-  def get(self, request, *args, **kwargs):
-    # Get the latest application from the database (modify as needed)
-    application = Application.objects.latest('id')
+    permission_classes = [IsAuthenticated]
 
-    if application:
-      data = {
-          "name": application.first_name + " " + application.last_name,  # Combine first and last name
-          "reason": application.reason,  # Add reason for applying
-          "created_at": application.created_at.strftime("%Y-%m-%d"),  # Format creation date
-          "id": application.pk,
-          "ref": "00" + str(application.pk)
-      }
-      pdf = render_to_pdf('report.html', data)
-      if pdf:
-          response = HttpResponse(pdf, content_type='application/pdf')
-          filename = "Application_for_%s.pdf" % (data['name'])
-          content = "inline; filename= %s" % (filename)
-          response['Content-Disposition'] = content
-          return response
-    return HttpResponse("No applications found")  # Inform user if no applications exist
+    def get(self, request, *args, **kwargs):
+        # Get the latest application from the database (modify as needed)
+        application = Application.objects.latest('id')
+
+        if application:
+            data = {
+                "name": application.name,
+                "phone_number": application.phone_number,
+                "occurrence_date_time": application.occurrence_date_time.strftime("%B %d, %Y at %I:%M %p"),
+                "address": application.address,
+                "lost_document": application.lost_document,
+                "other_document_details": application.other_document_details,
+                "created_at": application.created_at.strftime("%B %d, %Y"),
+                "id": application.pk,
+                "ref": "APP-2024-000" + str(application.pk),
+                "reason": f"{application.name} from {application.address}, on phone number {application.phone_number} reported that on {application.occurrence_date_time.strftime('%B %d, %Y')} at about {application.occurrence_date_time.strftime('%I:%M %p')}, he detected that his {application.get_lost_document_display()} could not be found. That all efforts made to trace same proved futile, hence needed police assistance. \n\nExtract of occurrence was prepared and issued to the complainant to be taken to Passport Office and Driver and Vehicle Licensing Authority (DVLA) respectively for further action."
+            }
+            pdf = render_to_pdf('report.html', data)
+            if pdf:
+                response = HttpResponse(pdf, content_type='application/pdf')
+                filename = "Application_for_%s.pdf" % (data['name'])
+                content = "inline; filename= %s" % (filename)
+                response['Content-Disposition'] = content
+                return response
+        return HttpResponse("No applications found")
 
 
 class ApplicationCreateView(APIView):
-  def get(self, request):
-    form = ApplicationSerializer()  # Create an empty form instance
-    return render(request, 'application_form.html', {'form': form})
+    permission_classes = [IsAuthenticated]
 
-  def post(self, request):
-    form = ApplicationSerializer(data=request.data)
-    if form.is_valid():
-        form.save()
-        return redirect('pdf') 
-    else:
-        return render(request, 'application_form.html', {'form': form})  # Render form with errors
+    def get(self, request):
+        form = ApplicationSerializer()  # Create an empty form instance
+        return render(request, 'application_form.html', {'form': form})
+
+    def post(self, request):
+        form = ApplicationSerializer(data=request.data)
+        if form.is_valid():
+            form.save()
+            return redirect('pdf') 
+        else:
+            return render(request, 'application_form.html', {'form': form})
