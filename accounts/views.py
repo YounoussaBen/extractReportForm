50ad59f9660d75fe8002fe2d2037c1539217d29a
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-21 15:06:00 +0000

    success message

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.messages import success
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        self.object = form.save()
+        success(self.request, message="Your account has been created successfully!")
+        return super().form_valid(form)
