951b705da99c1c3c2af86adf83527f0dad549e93
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-23 04:40:54 +0000

    Update on authentication

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.messages import success
-
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
 
     def form_valid(self, form):
         self.object = form.save()
         success(self.request, message="Your account has been created successfully!")
         return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password. Please try again.')
+        return super().form_invalid(form)
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        messages.success(self.request, 'You have successfully logged in.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password. Please try again.')
+        return super().form_invalid(form)
--- extractReportForm/urls.py
+++ extractReportForm/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    # path("accounts/", include("django.contrib.auth.urls")),
     path("download/", include("police_report.urls")),
      path("__reload__/", include("django_browser_reload.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
