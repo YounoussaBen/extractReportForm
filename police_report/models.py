f16d2adc5406ae31867c49b27a36051ddfaa55c0
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-03-24 22:53:59 +0000

    Update models

--- police_report/models.py
+++ police_report/models.py
@@ -1,30 +1,29 @@
 from django.db import models
 
 class Application(models.Model):
-    # User details
-    name = models.CharField(max_length=100)  # Combining first_name and last_name into a single field
-    phone_number = models.CharField(max_length=20)
-    occurrence_date_time = models.DateTimeField()
-    address = models.TextField()
+   # User details
+   name = models.CharField(max_length=100)  # Combining first_name and last_name into a single field
+   phone_number = models.CharField(max_length=20)
+   occurrence_date_time = models.DateTimeField()
+   house_number = models.CharField(blank=True, max_length=100) 
 
-    # Lost Document details
-    DOCUMENT_CHOICES = [
-        ('passport', 'Passport'),
-        ('drivers_license', "Driver's License"),
-        ('ghana_card', 'Ghana Card'),
-        ('voter_id_card', 'Voter ID Card'),
-    ]
-    lost_document = models.CharField(max_length=100, choices=DOCUMENT_CHOICES)
-    other_document_details = models.CharField(max_length=100, blank=True)
+   # Lost Document details
+   DOCUMENT_CHOICES = [
+       ('passport', 'Passport'),
+       ('drivers_license', "Driver's License"),
+       ('ghana_card', 'Ghana Card'),
+       ('voter_id_card', 'Voter ID Card'),
+   ]
+   lost_document = models.CharField(max_length=100, choices=DOCUMENT_CHOICES)
 
-    # Additional fields for document details
-    issue_date = models.DateField(default=None, blank=True)  # Set default to None and allow blank values
-    expiry_date = models.DateField(default=None, blank=True)  # Set default to None and allow blank values
-    identification_number = models.CharField(max_length=100, blank=True)  # Set default to empty string and allow blank values
+   # Additional fields for document details
+   issue_date = models.DateField(default=None, blank=True)  
+   expiry_date = models.DateField(default=None, blank=True)  
+   identification_number = models.CharField(max_length=100, blank=True)  
 
-    # Timestamps
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+   # Timestamps
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.name} - Application for {self.get_lost_document_display()}"
+   def __str__(self):
+       return f"{self.name} - Application for {self.get_lost_document_display()}"
