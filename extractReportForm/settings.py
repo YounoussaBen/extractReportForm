476e5aeb0a7ab7ec4ee7cf2e1aa73063386f9863
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-05-04 18:09:56 +0000

    Change email host password

--- extractReportForm/settings.py
+++ extractReportForm/settings.py
@@ -156,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ads21b00266y@ait.edu.gh'
-EMAIL_HOST_PASSWORD = 'pxfn rxip ecks ssmu'
+EMAIL_HOST_USER = 'policeservicereport@gmail.com'
+EMAIL_HOST_PASSWORD = 'fspd tdhj kxqx eswc'
