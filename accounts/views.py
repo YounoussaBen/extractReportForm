7bca7f4160d798b27f15117ca0cbc4428ec65b7e
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-05-04 10:44:07 +0000

    Update user

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.messages import success
+from django.contrib.messages import success, error
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
+from django import forms
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
 
@@ -17,8 +25,9 @@ class SignUpView(CreateView):
         return super().form_valid(form)
 
     def form_invalid(self, form):
-        messages.error(self.request, 'Invalid username or password. Please try again.')
+        error(self.request, 'Invalid username or password. Please try again.')
         return super().form_invalid(form)
+
 class CustomLoginView(LoginView):
     def form_valid(self, form):
         messages.success(self.request, 'You have successfully logged in.')
@@ -27,9 +36,8 @@ class CustomLoginView(LoginView):
     def form_invalid(self, form):
         messages.error(self.request, 'Invalid username or password. Please try again.')
         return super().form_invalid(form)
-    
+
 class CustomLogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, 'You have successfully logged out.')
         return super().dispatch(request, *args, **kwargs)
-    
